f8e3dc487b56aa6599dd296e9abfcebf2a8fdd9b
Risto-Matti Ratilainen <risto-matti.ratilainen@hiq.fi>
2018-03-09 11:38:12 +0200

    Configuration value helper component class created and basic test method also created. This is Initial version of the tool.

---
+++ MyDemoComponent1Tests/ConfigHandlerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestConfigurationHandler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConfigurationHandler.Tests
+{
+    [TestClass()]
+    public class ConfigHandlerTests
+    {
+        [TestMethod()]
+        public void ReadConfigValueTest()
+        {
+
+            var t = ConfigHandler.ReadConfigValue("Test");
+
+        }
+    }
+}
---
+++ TestConfigurationHandler/ConfigHandler.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConfigurationHandler
+{
+    public class ConfigHandler
+    {
+        private static string ConfigJSONstring = System.IO.File.ReadAllText(@"C:\Temp\VSTestConfiguration\Config.json");
+
+        public static object ReadConfigValue(string configkey)
+        {
+            //var des = JsonConvert.SerializeObject(new Configuration()
+            //{
+            //    configlist = new List<ConfigObject>()
+            //    { new ConfigObject(){ key = "x", value = "h"},
+            //      new ConfigObject(){ key = "y", value = "t"}
+            //    }
+
+            //});
+
+            var config = JsonConvert.DeserializeObject<Configuration>(ConfigJSONstring);
+            string configvalue = "";
+
+            configvalue = config.configlist.FirstOrDefault(c => c.key.ToLower() == configkey.ToLower()).value;
+
+            return configvalue;
+        }
+    }
+
+    public class Configuration
+    {
+        public List<ConfigObject> configlist;
+    }
+
+    public class ConfigObject
+    {
+       public  string key;
+       public  string value;
+    }
+}
